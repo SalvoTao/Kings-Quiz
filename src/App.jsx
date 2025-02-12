68df9e6dc4ff27e71d41b7179793daff1cd2734a
Salvatore Taormina <salvatore.taormina@consulenti.mediolanum.it>
2025-02-12 19:56:17 +0100

    Inizializzazione del progetto React

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Quiz Interattivo</h1>
+      <p>Benvenuto nel quiz! Presto vedrai la tabella delle domande.</p>
+    </div>
+  );
+}
+
+export default App;
